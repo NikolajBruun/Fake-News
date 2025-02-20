1430e04c6e550fa5ceaef3e7f8e8d83a528ffe8e
SarahGrabas <sarahgrabas2@gmail.com>
2025-02-20 15:43:05 +0100

    Gør det igen

--- cleaningNIKO.py
+++ cleaningNIKO.py
@@ -78,10 +78,7 @@ from cleantext import clean
 
 auto_cleaning_df = df[["content"]].copy()
 
-def remove_special_chars(x):
-    return re.sub(r"[^a-zA-Z0-9<>]", " ", x)
 
-auto_cleaning_df['remove_special_chars'] = auto_cleaning_df['content'].apply(remove_special_chars)
 
 def sub_dates(x):
     sub_dates = re.sub(r'\d{4}[-/]\d{2}[-/]\d{2}|\d{2}[-/]\d{2}[-/]\d{4}', ' <DATE> ', x)
@@ -89,14 +86,20 @@ def sub_dates(x):
     sub_dates = re.sub(r'\b\d{1,2}[,]?\s[a-zA-Z]+\s\d{4}\b', '<DATE>', sub_dates)
     return sub_dates
 
-auto_cleaning_df['sub_dates'] = auto_cleaning_df['remove_special_chars'].apply(sub_dates)
+auto_cleaning_df['sub_dates'] = auto_cleaning_df['content'].apply(sub_dates)
+def remove_special_chars(x):
+    return re.sub(r"[^a-zA-Z0-9<>]", " ", x)
+
+
+
+
 #auto_cleaning_df['cleaned'] = clean(auto_cleaning_df['content'], lower=True, no_urls=True, no_emails=True, no_numbers=True)
-auto_cleaning_df['cleaned'] = auto_cleaning_df['sub_dates'].apply(
+auto_cleaning_df['clean_with_specchars'] = auto_cleaning_df['sub_dates'].apply(
     lambda x: clean(x, lower=True, no_urls=True, no_emails=True, no_numbers=True)
 )
 
 
-
+auto_cleaning_df['cleaned'] = auto_cleaning_df['clean_with_specchars'].apply(remove_special_chars)
 
 #print(auto_cleaning_df['cleaned'].head(99))
 
@@ -113,18 +116,27 @@ auto_cleaning_df['filtered_tokens'] = auto_cleaning_df['tokens'].apply(lambda x:
     word for word in x if word.lower() not in stop_words and not any(tword in word.lower() for tword in ["number", "date", "url", "email", "<", ">"])
 ])
 
-# Vis de førsfte par rækker af de filtrerede tokens
 print(auto_cleaning_df['filtered_tokens'].head())
 
+samlet_tekst = ' '.join([' '.join(words) for words in auto_cleaning_df['filtered_tokens']])
 
+alle_ord = samlet_tekst.split()
 
+alltext = '\n'.join(df["content"])
+words=alltext.split()
+print("antal uncleaned words",len(set(words)))
+
+#print(samlet_tekst)
+#print(alle_ord)
 
 
 import matplotlib.pyplot as plt
 from collections import Counter 
 
-ord_antal=Counter(auto_cleaning_df)     
-#print(ord_antal)#Definerer 'ord_antal' 
+ord_antal=Counter(alle_ord)     
+print("Total wordcount after cleaning:",len(ord_antal))
+
+print("Procentsvis ændring:", (len(ord_antal)/len(set(words)))*100 )
 
 hypiggeste_ord = ord_antal.most_common(50)              #De 50 mest hyppige ord i 'ord_antal', laves til en liste mad tupler, 
                                                         #bestående af ord og ordet's hyppighed
@@ -135,11 +147,14 @@ antal = [tælling for ord, tælling in hypiggeste_ord]    #Laver en liste med an
                                                         #Her itereres gennem hver tuple og udtrækkes antal forekomster.
 
 #Plottet
-plt.figure(figsize=(100, 50))                           #Figurens højde og bredde
-plt.bar(ord, antal, color = "hotpink")                #'ord' og 'antal' sættes på x- og y-aksen i plottet
+plt.figure(figsize=(15, 6))                             #Figurens højde og bredde
+plt.bar(ord, antal, color = "hotpink")                  #'ord' og 'antal' sættes på x- og y-aksen i plottet
+plt.xticks(rotation=45, ha='right', fontsize=11)        #Teksten på x-aksen roteres og tekststørrelsen vælges
+#plt.show() 
 
-plt.tick_params(axis='y', labelsize=50)                 #Tekststørrelse på y-aksen
-plt.xticks(rotation=45, ha='right', fontsize=60)        #Teksten på x-aksen roteres og tekststørrelsen vælges
-plt.tight_layout()
-
-
+#=========================================================================================================
+#=========================================================================================================
+#=========================================================================================================
+#=========================================================================================================
+print("=================================================================================")
+#STEMMING!!!!
