cf13f71ddfe48e6bc7160d73bab8b832bcff0be1
NikolajBruun <thranebruun@gmail.com>
2025-03-04 14:23:52 +0100

    idk

--- cleaningNIKO.py
+++ cleaningNIKO.py
@@ -75,68 +75,51 @@ def remove_special_chars(x):
 # Stopwords fra NLTK
 stop_words = set(stopwords.words('english'))
 
-# Betingelse for at fjerne stopwords og de ord, vi ikke ønsker
-#auto_cleaning_df['filtered_tokens'] = auto_cleaning_df['tokens'].apply(lambda x: [
-  #  word for word in x if word.lower() not in stop_words and not any(tword in word.lower() for tword in ["number", "date", "url", "email", "<", ">"])
-#])
-
-def autocleandf(df):
-    df['sub_dates']=df['content']
-    df['clean_with_specchars']=df['sub_dates'].apply(
-        lambda x: clean(x, lower=True, no_urls=True, no_emails=True, no_numbers=True)
-    )
-    df['cleaned']=df['clean_with_specchars'].apply(remove_special_chars)
-    df['tokens']=df['cleaned'].apply(lambda x: word_tokenize(x))
-    df['filtered_tokens']=df['tokens'].apply(lambda x: [
-    word for word in x if word.lower() not in stop_words and not any(tword in word.lower() for tword in ["number", "date", "url", "email", "<", ">"])
-    ])
-autocleandf(auto_cleaning_df)
-#print("jegprinterher", auto_cleaning_df['filtered_tokens'].head())
-
-
-samlet_tekst = ' '.join([' '.join(words) for words in auto_cleaning_df['filtered_tokens']]) #gør til lang string
-alle_ord = samlet_tekst.split() #gør til liste
-alltext = '\n'.join(df["content"]) #gør raw content fil til en string
-words=alltext.split() #dernæst til lsite
-print("antal uncleaned words",len(set(words)))
 
 
 
-import matplotlib.pyplot as plt
-from collections import Counter 
 
-ord_antal=Counter(alle_ord)     
-print("Total wordcount after cleaning:",len(ord_antal))
-
-print("Procentsvis ændring:", (len(ord_antal)/len(set(words)))*100 )
-
-hypiggeste_ord = ord_antal.most_common(50)              #De 50 mest hyppige ord i 'ord_antal', laves til en liste mad tupler, 
-                                                        #bestående af ord og ordet's hyppighed
-
-ord = [ord for ord, tælling in hypiggeste_ord]          #Laver en liste med ordene fra 'hypiggeste_ord'. 
-                                                        #Her itereres gennem hver tuple og udtrækkes ordet.
-antal = [tælling for ord, tælling in hypiggeste_ord]    #Laver en liste med antal forekomster fra 'hypiggeste_ord'. 
-                                                        #Her itereres gennem hver tuple og udtrækkes antal forekomster.
+# samlet_tekst = ' '.join([' '.join(words) for words in auto_cleaning_df['filtered_tokens']]) #gør til lang string
+# alle_ord = samlet_tekst.split() #gør til liste
+# alltext = '\n'.join(df["content"]) #gør raw content fil til en string
+# words=alltext.split() #dernæst til lsite
+# print("antal uncleaned words",len(set(words)))
 
 
-#NOTE TO SELF LAV EVT PLOT DEL OM 200% CHAT
 
-#Plottet
-plt.figure(figsize=(15, 6))                             #Figurens højde og bredde
-plt.bar(ord, antal, color = "hotpink")                  #'ord' og 'antal' sættes på x- og y-aksen i plottet
-plt.xticks(rotation=45, ha='right', fontsize=11)        #Teksten på x-aksen roteres og tekststørrelsen vælges
-plt.show() 
+# import matplotlib.pyplot as plt
+# from collections import Counter 
 
-#=========================================================================================================
-#=========================================================================================================
-#=========================================================================================================
-#=========================================================================================================
-print("=================================================================================")
-print("=================================================================================")
-#STEMMING!!!!
+# ord_antal=Counter(alle_ord)     
+# print("Total wordcount after cleaning:",len(ord_antal))
 
-ps=PorterStemmer()
+# print("Procentsvis ændring:", (len(ord_antal)/len(set(words)))*100 )
 
-auto_cleaning_df['stemmed_text']=auto_cleaning_df['cleaned'].apply(lambda x: ps.stem(x))
+# hypiggeste_ord = ord_antal.most_common(50)              #De 50 mest hyppige ord i 'ord_antal', laves til en liste mad tupler, 
+#                                                         #bestående af ord og ordet's hyppighed
 
-print(auto_cleaning_df['stemmed_text'].head(50))
+# ord = [ord for ord, tælling in hypiggeste_ord]          #Laver en liste med ordene fra 'hypiggeste_ord'. 
+#                                                         #Her itereres gennem hver tuple og udtrækkes ordet.
+# antal = [tælling for ord, tælling in hypiggeste_ord]    #Laver en liste med antal forekomster fra 'hypiggeste_ord'. 
+#                                                         #Her itereres gennem hver tuple og udtrækkes antal forekomster.
+
+
+# #NOTE TO SELF LAV EVT PLOT DEL OM 200% CHAT
+
+# #Plottet
+# plt.figure(figsize=(15, 6))                             #Figurens højde og bredde
+# plt.bar(ord, antal, color = "hotpink")                  #'ord' og 'antal' sættes på x- og y-aksen i plottet
+# plt.xticks(rotation=45, ha='right', fontsize=11)        #Teksten på x-aksen roteres og tekststørrelsen vælges
+# plt.show() 
+
+# #=========================================================================================================
+# #=========================================================================================================
+# #=========================================================================================================
+# #=========================================================================================================
+# print("=================================================================================")
+# print("=================================================================================")
+# #STEMMING!!!!
+
+
+
+# print(auto_cleaning_df['stemmed_text'].head(50))
